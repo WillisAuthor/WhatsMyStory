439adf08cee4d5b5323aeaf1b38008fe594a6cdf
WillisAuthor <100448459+WillisAuthor@users.noreply.github.com>
2022-02-25 20:21:09 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -36,7 +36,7 @@ $(function() {
 
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    // WTF.init( '0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc' );
+     WTF.init( '0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc' );
     // WTF.init( '1ETfPSeCQJoLRRkqQQ6b8P4hvVJ8UrXbuNFZ9SRUqOsA' );
-       WTF.init( '2PACX-1vR-vpMhRmASTY0RAud6t_WEWfHjsqCaBOL0JmklB2kEStUS5ChzDewI64ncJjaKOxeP6ufa0vFlNpkx' );
+    // WTF.init( '2PACX-1vR-vpMhRmASTY0RAud6t_WEWfHjsqCaBOL0JmklB2kEStUS5ChzDewI64ncJjaKOxeP6ufa0vFlNpkx' );
 });
