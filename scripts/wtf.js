b79f0f042fed0ae5522d79245b451d478c082cda
WillisAuthor <100448459+WillisAuthor@users.noreply.github.com>
2022-02-25 22:38:47 -0500

    Update wtf.js

--- scripts/wtf.js
+++ scripts/wtf.js
@@ -15,8 +15,7 @@ var WTF = (function() {
     var RE_JSON = /\.json$/i;
     var RE_COL = /^gsx\$(.+)$/i;
     var RE_KEY = /[a-z0-9_-]{32,}/i;
-    //var DOCS_PATH = "https://spreadsheets.google.com/feeds/list/{key}/od6/public/values?alt=json";
-    var DOCS_PATH = "https://docs.google.com/spreadsheets/d/{key}/export?gid=0&format=csv";
+    var DOCS_PATH = "https://spreadsheets.google.com/feeds/list/{key}/od6/public/values?alt=json";
 
 
     var templates;
