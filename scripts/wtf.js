61f6959054455b98253dbab78943f6a080a8f1d1
WillisAuthor <100448459+WillisAuthor@users.noreply.github.com>
2022-02-25 22:59:21 -0500

    Update wtf.js

--- scripts/wtf.js
+++ scripts/wtf.js
@@ -369,3 +369,12 @@ var WTF = (function() {
     };
 
 })();
+
+
+const reloadtButton = document.querySelector("#reload");
+// Reload everything:
+function reload() {
+    reload = location.reload();
+}
+// Event listeners for reload
+reloadButton.addEventListener("click", reload, false);
